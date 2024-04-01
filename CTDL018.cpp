41adb8f3a48e9b86272e5c3ba1800ccd16c07b72
Doan Van Diep <147433897+doanvandiep@users.noreply.github.com>
2024-04-01 16:24:50 +0700

    Update CTDL018.cpp

--- CTDL018.cpp
+++ CTDL018.cpp
@@ -1,21 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<vector<int>> check(vector<int> &v)
+int n;
+int a[15];
+void in()
 {
-	vector<vector<int>> result;
-	result.push_back(v);
-	vector<int> result1=v;
-	while(result1.size()>1)
+	cout << "[";
+	for(int i=0;i<n-1;i++)
 	{
-		vector<int> result2;
-		for(int i=0;i<result1.size()-1;i++)
-		{
-			result2.push_back(result1[i]+result1[i+1]);
-		}
-		result.push_back(result2);
-		result1=result2;
+		cout << a[i] << " ";
 	}
-	return result;
+	cout << a[n-1] << "]" << endl;
+}
+void Try()
+{
+	for(int i=0;i<n;i++)
+	{
+		cin >> a[i];
+	}
+	in();
+	n--;
+	while(n>0)
+	{
+		for(int i=0;i<n;i++)
+		{
+			a[i]=a[i]+a[i+1];
+		}
+		in();
+		n--;
+	}
 }
 int main()
 {
@@ -23,24 +35,7 @@ int main()
 	cin >> t;
 	while(t--)
 	{
-		int n;
 		cin >> n;
-		vector<int> v(n);
-		for(int i=0;i<n;i++)
-		{
-			cin >> v[i];
-		}
-		vector<vector<int>> result=check(v);
-		for(const auto &row:result)
-		{
-			cout <<"[";
-			for(int i=0;i<row.size();i++)
-			{
-			    cout << row[i];
-			    if(i!=row.size()-1) cout <<" ";
-			}
-			cout <<"]";
-			cout << endl;
-		}
+		Try();
 	}
 }
